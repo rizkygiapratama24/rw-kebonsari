7f71eeb64c1075a4c1dd04ec24b0469cab452a47
Rizky Giapratama <54215455+rizkygiapratama24@users.noreply.github.com>
2022-12-09 10:24:29 +0700

    update calendar public

--- ronda.php
+++ ronda.php
@@ -11,20 +11,9 @@
       <!-- Font Awesome -->
       <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css">
       <!-- Calendar CSS -->
-      <link rel="stylesheet" href="assets/vendor/fullcalendar/lib/main.css">
+      <link rel="stylesheet" href="assets/vendor/fullcalendar-3.4.0/fullcalendar.css">
       <!-- Style CSS -->
       <link rel="stylesheet" href="assets/css/style.css">
-      <style>
-        .btn-info.text-light:hover,
-        .btn-info.text-light:focus {
-            background: #000;
-        }
-        table, tbody, td, tfoot, th, thead, tr {
-            border-color: #ededed !important;
-            border-style: solid;
-            border-width: 1px !important;
-        }
-      </style>
     </head>
   <body>
     
@@ -39,55 +28,87 @@
         </div>
     </section>
 
-    <!-- Event Details Modal -->
-    <div class="modal fade" tabindex="-1" data-bs-backdrop="static" id="event-details-modal">
-        <div class="modal-dialog modal-dialog-centered">
+    <!-- Modal -->
+    <div class="modal fade" id="CalendarModal" tabindex="-1" aria-labelledby="CalendarModalLabel" aria-hidden="true">
+        <div class="modal-dialog">
             <div class="modal-content">
-                <div class="modal-header">
-                    <h5 class="modal-title">Detail Jadwal Ronda</h5>
-                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                </div>
-                <div class="modal-body">
-                    <div class="container-fluid">
-                        <dl>
-                            <dt class="text-muted">Judul</dt>
-                            <dd id="title" class="fw-bold fs-4"></dd>
-                            <dt class="text-muted">Deskripsi</dt>
-                            <dd id="description" class=""></dd>
-                            <dt class="text-muted">Mulai</dt>
-                            <dd id="start" class=""></dd>
-                            <dt class="text-muted">Selesai</dt>
-                            <dd id="end" class=""></dd>
-                        </dl>
-                    </div>
-                </div>
+            <div class="modal-header">
+                <h1 class="modal-title fs-5" id="CalendarModalLabel">Detail</h1>
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <div class="modal-body">
+                <strong>Judul Ronda : </strong>
+                <p id="title"></p>
+                <strong>Anggota Ronda : </strong>
+                <p id="description"></p>
+                <strong>Tanggal & Waktu Mulai Ronda : </strong>
+                <p id="start"></p>
+                <strong>Tanggal & Waktu Selesai Ronda : </strong>
+                <p id="end"></p>
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                <button type="button" class="btn btn-primary">Save changes</button>
+            </div>
             </div>
         </div>
-    </div>
-    <!-- Event Details Modal -->
+    </div>   
 
     <?php include ('layouts/footer.php') ?>
-
-    <?php 
-      $schedules = $db->query("SELECT * FROM schedule_list");
-      $sched_res = [];
-      foreach($schedules->fetch_all(MYSQLI_ASSOC) as $row){
-          $row['sdate'] = date("F d, Y h:i A",strtotime($row['start_datetime']));
-          $row['edate'] = date("F d, Y h:i A",strtotime($row['end_datetime']));
-          $sched_res[$row['id']] = $row;
-      }
-    ?>
-    <?php 
-      if(isset($db)) $db->close();
-    ?>
     
-    <script src="https://code.jquery.com/jquery-3.5.1.js"></script>
+    <script src="assets/vendor/fullcalendar-3.4.0/lib/jquery.min.js"></script>
+    <script src="assets/vendor/fullcalendar-3.4.0/lib/moment.min.js"></script>
+    <script src="assets/vendor/fullcalendar-3.4.0/fullcalendar.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
     <script src="assets/vendor/bootstrap/dist/js/bootstrap.min.js"></script>
-    <script src="assets/vendor/fullcalendar/lib/main.js"></script>
     <script src="assets/js/custom.js"></script>
     <script>
-        var scheds = $.parseJSON('<?= json_encode($sched_res) ?>')
+        $(function() {
+
+          var calendar = $('#calendar').fullCalendar({
+              header:{
+                  left:'prev,next today',
+                  center:'title',
+                  right:'month,agendaWeek,agendaDay'
+              },
+              events: 'load_schedule.php',
+              timeFormat: 'H(:mm)',
+              eventClick: function(event,jsEvent, view) {
+                  var modal = $('#CalendarModal');
+                  modal.find('#title').text(event.title);
+                  modal.find('#description').text(event.description);
+                  modal.find('#start').text(moment(event.start).format('DD MMMM YYYY HH:mm A'));
+                  modal.find('#end').text(moment(event.end).format('DD MMMM YYYY HH:mm A'));
+                  modal.modal('show');
+              },
+              eventRender: function(event,element) {
+                  element.find('.fc-title').append("<br/> Anggota : <br/>" + event.description);
+              },
+              selectable:true,
+              selectHelper:true,
+          })
+
+          // $('#calendar').fullCalendar({
+          //     events: 
+          //     [ 
+          //         { 
+          //             id: 1, 
+          //             title: 'First Event', 
+          //             start: '2022-12-09',
+          //             description: 'first description' 
+          //         }, 
+          //         { 
+          //             id: 2, 
+          //             title: 'Second Event', 
+          //             start: '2022-12-10',
+          //             description: 'second description'
+          //         }
+          //     ], 
+          //     eventRender: function(event, element) { 
+          //         element.find('.fc-title').append("<br/>" + event.description); 
+          //     } 
+          // })
+          })
     </script>
   </body>
 </html>
